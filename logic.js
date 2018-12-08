585be4f0d05510f576a69a3702a1d09b7757f095
Axel <axelpriven@gmail.com>
2018-12-08 16:06:28 +0200

    style done

--- logic.js
+++ logic.js
@@ -1,32 +1,65 @@
 class App extends React.Component {
     constructor(props) {
         super(props)
+        this.counter = 0;
         this.new = []
         this.state = {
-            show: 'none',
             what: '',
-            date: ''
+            date: '',
+            new: []
         }
+        this.addNewItem = this.addNewItem.bind(this);
+    }
+
+    addNewItem() {
+        this.counter ++;
+        console.log(this.counter)
+        var activity = [<Item id={`num${this.counter}`} what={this.what.value} date={this.date.value}/>]
+        this.new.push(activity)
+        this.setState({
+            new: this.new
+        })
     }
     render() {
         return (
             <div>
-                <input placeholder='my new activity' ref={(input) => { this.textInput = input; }} className='input' />
-                <input type='date' ref={(input) => { this.date = input; }}/>
-                <button onClick={() => {
-                    this.setState({
-                        what: this.textInput.value
-                    })
-                    this.setState({
-                        date: this.date.value
-                    })
-                    this.new.push(<li className='list'>{`${this.state.what} on ${this.state.date}`}</li>);
-                }}>Add</button>
+                <div className ="header">
+                    <div id="toad"></div>
+                    <div className="name">TOAD LIST</div>
+                    <div id="toad"></div>
+                </div>
+                <input placeholder='my new activity' ref={x => this.what = x} className='input' />
+                <input type='date' className="date" ref={x => this.date = x} />
+                <button onClick={this.addNewItem}className='add'>🍄</button>
                 <ul>
-                    {this.new}
+                    {this.state.new}
                 </ul>
             </div>
         );
+    }
+}
+
+class Item extends React.Component {
+    constructor(props) {
+        super (props)
+        this.doneList = [];
+        this.state = {
+            done : ''
+        }
+        this.done = this.done.bind(this)
+    }
+    done(e) {
+        var checked = this.state.done == '' ? 'line-through' : '';
+        this.setState({
+            done: checked
+        })
+        this.doneList.push(`${this.props.what}&nbsp;on&nbsp;${this.props.date}`);
+        console.log(this.doneList)
+    }
+    render() {
+        return (
+            <li  className = "list" style={{'textDecoration':this.state.done}} type='none'><div className="pagestyle"></div><input onClick={this.done} type='checkbox'/>{this.props.what}&nbsp;on&nbsp;{this.props.date}</li>
+        )
     }
 }
 ReactDOM.render(
